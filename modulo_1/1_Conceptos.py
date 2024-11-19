ceb60796313e96fea68fcc14796a7606908a89b7
Joseph Code <pepet_21@hotmail.com>
2024-11-19 19:37:23 +0100

    Conceptos

---
+++ modulo_1/1_Conceptos.py
@@ -0,0 +1,37 @@
+""" 
+El análisis de datos es el proceso de inspeccionar, limpiar, transformar y modelar datos con el objetivo de
+descubrir información útil, llegar a conclusiones y apoyar la doma de decisiones informadas. Es una
+disciplina esencial en numerosos compos como negocios, ciencia, ingeniería y más.
+
+Principales etapas Del Análisis de Datos:
+1. Recolección de datos: Obtención de datos de diversas fuentes.
+2. Limpieza de datos: Corrección o eliminación de datos incorrectos o irrelevantes.
+3. Exploración de datos: Análisis inicial para descubrir patrones, detectar anomalías y verificar supuestos.
+4. Modelado de Datos: Aplicación de modelos estadísticos o de aprendizaje automático.
+5. Interpretación y Comunicación: Presentación de los resultados de manera clara y concisa.
+
+Tipos de Datos:
+1. Estructurados: Datos organizados en un formato definido como tablas (por ejemplo, bases de datos relacionales).
+2. No estructurados: Datos sin una estructura predefinida (por ejemplo, texto libre, imágenes, videos).
+3. Semiestructurados: Datos que no están en tablas pero tienen etiquetas o marcadores que los separan(por ejemplo,XML,JSON).
+
+Herramientas Comunes:
+1. Lenguajes de programación: Python, R.
+2. Bibliotecas para Python: Pandas, Numpy, Matplotib, Seaborn.
+3. Software de Visualización: Tableau, Power BI.
+Bases de Datos: SQL
+"""
+
+""" 
+Ejercicio 1:
+Define con tus propias palabras qué es el análisis de datos y por qué es importante en el mundo actual.
+
+El análisis de datos es el procedo de examinar, organizar e interpretar conjuntos de datos para descubrir patrones, tendencias,
+relaciones y obtener información útil que ayude en la toma de decisiones. Es importante porque vivimos en un mundo cada vez
+más impulsado por datos: empresas, gobiernos, y organizaciones los recopilan constantemente para optimizar procesos, mejorar
+productos o servicios y tomar decisiones estratégicas.
+
+Por ejemplo, en el comercio eléctronico, los datos permiten entender los hábitos de compra de los clientes, lo que ayuda a
+personalizar experiencias de usuario y aumentar las ventas. En la salud, el análisis de datos se utiliza para detectar 
+enfermedades, predecir brotes y mejorar tratamientos.
+""" 
